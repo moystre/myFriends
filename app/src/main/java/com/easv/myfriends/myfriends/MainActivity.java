0c6e5bc6c60502d7c52617775e5178bcd0465049
GiorgioKozmani <mieszko.kozma@gmail.com>
2018-03-16 23:42:48 +0100

    FriendsAdapter added (needs some care), friendlist row layout added, added few lines in mainactivity -> wrote few TODOes.

--- app/src/main/java/com/easv/myfriends/myfriends/MainActivity.java
+++ app/src/main/java/com/easv/myfriends/myfriends/MainActivity.java
@@ -1,13 +1,77 @@
 package com.easv.myfriends.myfriends;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.easv.myfriends.myfriends.adapter.FriendsAdapter;
+import com.easv.myfriends.myfriends.model.Friend;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Friend> friendsList = new ArrayList<>();
+    ListAdapter adapter = null;
+    ListView listView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO POPULATE FRIENDSLIST
+        adapter = new FriendsAdapter(this, friendsList);
+        listView = (ListView) findViewById(R.id.friendsListView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+    public boolean onItemLongClick(AdapterView parent, View view, final int position, long id)
+    {
+        //  Item selectedItem = shoppingList.get(position);
+        if (!removeElement(position))
+            Toast.makeText(getApplicationContext(), "Error occured during removing friend", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+});
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+public void onItemClick(AdapterView parent, View view, final int position, long id)
+        {
+        Friend selectedItem = friendsList.get(position);
+        // TODO ADD ID TO FRIEND CLASS SO I CAN SEND IT IN INTENT AS EXTRAS AND START NEW ACTIVITY. cya im too drunk.
+        }
+
+        });
+    }
+
+
+    public boolean removeElement (final int position)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Remove Friend?");
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                friendsList.remove(position);
+                //  ??  Collections.sort(shoppingList, Item.COMPARE_BY_NAME);
+                //TODO SQL LOGIC FOR REMOVING (CREATE REPOSITORY FOR SQL ACCESS PURPOSE, NOTIFY ADAPTER ABOUT CHANGES
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+        return true;
     }
 }
---
+++ app/src/main/java/com/easv/myfriends/myfriends/adapter/FriendsAdapter.java
@@ -0,0 +1,69 @@
+package com.easv.myfriends.myfriends.adapter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.easv.myfriends.myfriends.R;
+import com.easv.myfriends.myfriends.model.Friend;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class FriendsAdapter extends BaseAdapter {
+
+    private LayoutInflater lInflater;
+    private Context mContext;
+    private ArrayList<Friend> friends = new ArrayList<>();
+
+    public FriendsAdapter(Context c, ArrayList<Friend> friends) {
+        mContext = c;
+        this.friends = friends;
+    }
+
+    public int getCount() {
+        return friends.size();
+    }
+    public int AAAAAAAAA() {
+        return friends.size();
+    }
+
+    public Friend getItem(int position) {
+        return friends.get(position);
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (convertView == null) {
+            view = lInflater.inflate(R.layout.friendslist_row, parent, false);
+        }
+        Friend friend = getItem(position);
+        // TODO ((ImageView) view.findViewById(R.id.friendListImageView)).set???  IF IMAGE THEN IMAGE ELSE
+        // TODO IMAGE PROMPT DEFAULT PICTURE);
+        ((TextView) view.findViewById(R.id.friendListName)).setText(friend.getmName());
+        ((TextView) view.findViewById(R.id.friendListPhone)).setText(friend.getmPhoneNumber());
+        ((TextView) view.findViewById(R.id.friendListAddress)).setText(friend.getmAddress());
+
+        return view;
+    }
+
+    public void removeItem(int position){
+        friends.remove(position);
+        notifyDataSetChanged();
+    }
+
+
+
+}
