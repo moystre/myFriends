2ea710c37b688ece3e710208948660fa6179dbee
moystre <moystre@gmail.com>
2018-04-08 23:21:42 +0200

    call() implemented

--- app/src/main/java/com/easv/myfriends/myfriends/DetailsActivity.java
+++ app/src/main/java/com/easv/myfriends/myfriends/DetailsActivity.java
@@ -1,7 +1,11 @@
 package com.easv.myfriends.myfriends;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -115,7 +119,11 @@ public class DetailsActivity extends AppCompatActivity {
     public void showHomeAddressLocation(View v){}
 
     // calling a friend's number with built-in application
-    public void call(View v){}
+    public void call(View v){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:0123456789"));
+        startActivity(intent);
+    }
 
     // sending an sms with built-in application
     public void sendSms(View v) {}
